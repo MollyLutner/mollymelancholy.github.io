85c6de26340e8c74e097d93caaacc1cde0912366
Molly Lutner <mollylutner@yahoo.co.uk>
2021-04-09 15:08:07 +0100

    added prev + back button hiding/showing

--- js/assessment.js
+++ js/assessment.js
@@ -1,15 +1,21 @@
 var page = 1;
 
 const showPage = () => {
-    document.querySelectorAll(".questions").forEach((question) =>{
-        question.hidden=true;
+    document.getElementById("questions").childNodes.forEach((question) => {
+        question.hidden = true;
     });
-    document.getElementById(page).hidden=false
+    document.getElementById(page).hidden = false;
+
 }
 const onNextClick = (maxPages) => {
     if (page < maxPages) {
         page = page + 1;
         showPage();
+        document.getElementById("prev-btn").disabled = false;
+        if (page == maxPages) {
+            document.getElementById("nxt-btn").disabled = true;
+            document.getElementById("submit").hidden = false;
+        }
     }
     
 }
@@ -17,5 +23,10 @@ const onPrevClick = () => {
     if (page > 1) {
         page = page - 1;
         showPage();
+        document.getElementById("nxt-btn").disabled = false;
+        document.getElementById("submit").hidden = true;
+        if (page == 1) {
+            document.getElementById("prev-btn").disabled = true;
+        }
     }
 }
